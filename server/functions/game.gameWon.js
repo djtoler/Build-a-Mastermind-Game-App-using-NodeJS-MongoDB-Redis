cbcea62d3e71295f3a979aec7b40a6108f34e73f
Dwayne Toler (he/him) <84260551+djtoler@users.noreply.github.com>
2022-10-16 11:42:20 -0400

    Update game.gameWon.js

--- server/functions/game.gameWon.js
+++ server/functions/game.gameWon.js
@@ -1,17 +1,17 @@
-const didUserGuessAll4NumbersCorrect = async () => {
-    userGuessedAll4NumbersCorrect =
-      guess == randomNumberFromRequestBodyString ? true : false;
+// const didUserGuessAll4NumbersCorrect = async () => {
+//     userGuessedAll4NumbersCorrect =
+//       guess == randomNumberFromRequestBodyString ? true : false;
 
-    if (userGuessedAll4NumbersCorrect) {
-      const user = await User.findOne({ email: currentUserEmail });
-      user.alltime_games_won = user.alltime_games_won + 1;
-      user.save();
-      currentGame.game_won = true;
-      currentGame.total_points = 20000;
-      currentGame.total_correct_locations = 4;
-      currentGame.total_correct_numbers = 4;
-      currentGame.save();
-      return currentGame;
-    }
-    return userGuessedAll4NumbersCorrect;
-  };
+//     if (userGuessedAll4NumbersCorrect) {
+//       const user = await User.findOne({ email: currentUserEmail });
+//       user.alltime_games_won = user.alltime_games_won + 1;
+//       user.save();
+//       currentGame.game_won = true;
+//       currentGame.total_points = 20000;
+//       currentGame.total_correct_locations = 4;
+//       currentGame.total_correct_numbers = 4;
+//       currentGame.save();
+//       return currentGame;
+//     }
+//     return userGuessedAll4NumbersCorrect;
+//   };
